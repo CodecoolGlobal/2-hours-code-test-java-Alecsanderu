58a2d896121b623315970e99f79ef712b11156df
alecsanderu <oanceaalexander@yahoo.com>
2020-07-03 11:58:04 +0300

    submit and evaluate to implemet

--- src/main/java/com/mycompany/interviews/AnagramGame.java
+++ src/main/java/com/mycompany/interviews/AnagramGame.java
@@ -1,30 +1,46 @@
 package com.mycompany.interviews;
 
+import java.util.*;
+
 // TODO: Implement this class
 public class AnagramGame implements IAnagramGame
 {
-
+	String baseWord;
+	IWordDictionary dictionary;
+	public List<String> scoreList = new ArrayList<>();
 
 	public AnagramGame(String baseWord, IWordDictionary dictionary)
 	{
+		this.baseWord = baseWord;
+		this.dictionary = dictionary;
 	}
 
 	public void submitWord(String word)
 	{
+
 	}
 
 
 	public int evaluateWord(String word)
 	{
-	}
+		
+ 	}
 
 
 	public int getScoreAtPosition(int position)
 	{
+		if (position < scoreList.size()) {
+			return scoreList.get(position).length();
+		}
+		return -1;
 	}
 
 
 	public String getWordAtPosition(int position)
 	{
+		if (position < scoreList.size()) {
+			return scoreList.get(position);
+		}
+		return null;
 	}
 }
